9dd7a4bc3586fca5566a03c7add58be817c0068f
Saimonfaun <128052164+Saimonfaun@users.noreply.github.com>
2024-07-25 10:39:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,14 +8,15 @@ const perguntas = [
     { enunciado: "Você conhece todas as lendas do folclore brasileiro?", alternativas:[ {texto:"sim",afirmacao:"s"},{texto:"não",afirmacao:"n"} ]},
     { enunciado: "Deseja conhecer?",  alternativas:[ {texto:"sim",afirmacao:"s"},{texto:"não",afirmacao:"n"} ] },
     { enunciado: "Aqui estão algumas referências.https://www.todamateria.com.br/folclore-brasileiro/ (copie o link e coloque na url) Agora que você sabe, deseja continuar?",  alternativas:[ {texto:"sim",afirmacao:"s"},{texto:"não",afirmacao:"n"} ] },
-    { enunciado: "É uma pena ",alternativas:[ {texto:"voltar",afirmacao:"v"}]},
+    { enunciado: "A cuca vai te pegar!!! ",alternativas:[ {texto:"voltar",afirmacao:"v"}]},
     { enunciado: "Tem certeza?",  alternativas:[ {texto:"sim",afirmacao:1},{texto:"não",afirmacao:2} ] }, 
-    { enunciado: "Dentre essas lendas, quais você prefere?",alternativas:[ {texto:"CUCA",afirmacao:"c"},{texto:"SACI",afirmacao:2},{texto:"BOI TATÁ",afirmacao:3} ] },
+    { enunciado: "Dentre essas lendas, quais você prefere?",alternativas:[ {texto:"CUCA",afirmacao:"c"},{texto:"SACI",afirmacao:"S"},{texto:"BOI TATÁ",afirmacao:"B"} ] },
     { enunciado: "Aponte a principal característica do Saci",alternativas:[ {texto:"Tem uma só perna",afirmacao:1},{texto:"Sequestra crianças",afirmacao:2},{texto:"cobra de fogo com inúmeros olhos",afirmacao:3}]},
     { enunciado: "Aponte a principal característica do Boi Tatá",alternativas:[ {texto:"Tem uma só perna",afirmacao:1},{texto:"Sequestra crianças",afirmacao:2},{texto:"cobra de fogo com inúmeros olhos,",afirmacao:3} ] },
     { enunciado: "Aponte a principal característica da cuca",alternativas:[ {texto:"Tem uma só perna",afirmacao:1},{texto:"Sequestra crianças",afirmacao:2},{texto:"cobra de fogo com inúmeros olhos,",afirmacao:3} ] },
-    { enunciado: "Parabéns, você acertou! Deseja continuar?", alternativas:[ {texto:"sim",afirmacao:1},{texto:"não",afirmacao:2} ] },
-    { enunciado: "Uma pena, você errou. Deseja continuar?",alternativas:[ {texto:"sim",afirmacao:1},{texto:"não",afirmacao:2} ] },
+    { enunciado: "Parabéns, você acertou! Deseja refazer?", alternativas:[ {texto:"sim",afirmacao:1},{texto:"não",afirmacao:2} ] },
+    { enunciado: "Uma pena, você errou. Deseja refazer?",alternativas:[ {texto:"sim",afirmacao:1},{texto:"não",afirmacao:2} ] },
+    
 ];
 
 let atual = 0;
@@ -51,9 +52,31 @@ function respostaSelecionada(opcaoSelecionada) {
         atual = 0;
     }else if(atual ===5 && afirmacoes === "c"){
         atual = 8;
-    }else if(atual ===5 && afimações === 2 ){
+    }else if(atual ===5 && afirmacoes === "S" ){
         atual = 6;
-    } else {
+    } else if(atual===5 && afirmacoes === "B" ){
+        atual = 7;
+    }else if(atual===6 && afirmacoes === 1 ){
+        atual= 9;
+    }else if(atual ===6 && afirmacoes=== 2 || atual===6 && afirmacoes===3){
+        atual=10;
+    }else if(atual===7 && afirmacoes === 3){
+        atual= 9;
+    }else if(atual ===6 && afirmacoes=== 1 || atual===6 && afirmacoes===2){
+        atual=10;
+    }else if(atual===8 && afirmacoes === 2){
+        atual= 9;
+    }else if(atual ===8 && afirmacoes=== 1 || atual===6 && afirmacoes===3){
+        atual=10;
+    }else if (atual===9 && afirmacoes ===1){
+        atual=5;
+    }else if(atual ===9 && afirmacoes ===2){
+        atual=3;
+    }else if(atual===10 && afirmacoes ===1){
+        atual=5;
+    }else if(atual===10 && afirmacoes ===2){
+        atual=3;
+    }else {
         atual++;
     }
     
